75c64529ce8684de8bcc3f72a4dc6155096968bc
sharinisri-sketch <sharinisri163@gmail.com>
2026-04-20 07:46:42 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -40,8 +40,6 @@ activities = {
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
     }
 }
-
-
 @app.get("/")
 def root():
     return RedirectResponse(url="/static/index.html")
@@ -61,7 +59,9 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Get the specific activity
     activity = activities[activity_name]
-
+# Validate student is not already signed up
+    if email in activity["participants"]:
+        raise HTTPException(status_code=400, detail="Student is already signed up for this activity")
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
